b5df6c3b9693655ef47c1dedb9a27cfc101b8e37
Jorge Vargas <jva.2794.jv@gmail.com>
2016-11-04 02:15:05 -0500

    fixed first bug

--- src/vida/controller/VIDA.java
+++ src/vida/controller/VIDA.java
@@ -5,6 +5,9 @@
  */
 package vida.controller;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -19,19 +22,29 @@ import javafx.stage.Stage;
 public class VIDA extends Application {
     
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) {
         
-        GridPane all = new GridPane();
-        
-        Parent FarmWorker = FXMLLoader.load(getClass().getResource("/vida/View/FarmWorker/MainFarmer.fxml"));
-        Parent CrewLead = FXMLLoader.load(getClass().getResource("/vida/View/CrewLeaders/MainCrewLead.fxml"));
-        Parent FarmOwner = FXMLLoader.load(getClass().getResource("/vida/View/FarmOwner/MainFarmer.fxml"));
-        
-        all.addRow(0, FarmWorker, CrewLead, FarmOwner);
-        Scene scene = new Scene(all);
-        
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        try {
+            GridPane all = new GridPane();
+            System.out.println("I just created the GridPane");
+            
+            Parent FarmWorker = FXMLLoader.load(getClass().getResource("/vida/View/FarmWorker/MainFarmer.fxml"));
+            System.out.println("I created the Parent for MainFarmer");
+            Parent CrewLead = FXMLLoader.load(getClass().getResource("/vida/View/CrewLeaders/MainCrewLead.fxml"));
+            System.out.println("I created the Parent for CrewLead");
+            Parent FarmOwner = FXMLLoader.load(getClass().getResource("/vida/View/FarmOwner/MainFarmOwner.fxml"));
+            System.out.println("I created the Parent for FarmOwner");
+            
+            all.addColumn(0, FarmWorker, CrewLead, FarmOwner);
+            System.out.println("I just made a row");
+            Scene scene = new Scene(all);
+            System.out.println("I made the scene");
+            
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
     /**
